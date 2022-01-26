18e7bd2c613486254a0b78bb41e4e4bc61502474
Danielskauge <2001leinad@gmail.com>
2022-01-26 17:52:29 +0100

    ting

---
+++ cpp/CMakeFiles/ZED_Point_Cloud_Mapping.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ascend/spatial_mapping/ZED_spatial_mapping_testing/cpp/src/GLViewer.cpp" "/home/ascend/spatial_mapping/ZED_spatial_mapping_testing/cpp/CMakeFiles/ZED_Point_Cloud_Mapping.dir/src/GLViewer.o"
+  "/home/ascend/spatial_mapping/ZED_spatial_mapping_testing/cpp/src/main.cpp" "/home/ascend/spatial_mapping/ZED_spatial_mapping_testing/cpp/CMakeFiles/ZED_Point_Cloud_Mapping.dir/src/main.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/zed/include"
+  "/usr/local/cuda/include"
+  "include"
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
